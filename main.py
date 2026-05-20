e4bb29f578ae16f966ca056932ccccd3c7dcf6cf
vyhuynh147 <huynhyenvy140709@gmail.com>
2026-05-19 23:47:26 -0400

    still working

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-# 
+# Clock class
 class Clock:
+    # Constructor method
+    # color and number is private prperties. 
     def __init__(self, time, number, color):
         self.time = time
-        self.number = number
+        self.__number = number
         self.__color = color
+    #get the number
+    def get_number(self):
+        return self.__number
+    
+    #get color and its return value
+    def get_color(self):
+        return self.__color
     
     def set_Alarm(self):
         self.time +=1
-        # print(f"{self.number} stop the clock.")
+        print("Clock stopped and alarm is set.")
 
     def ringer(self):
-        print(f"The bell ringer have{self.__color} color. And total {self.time} hours.")
+        print(f"The bell ringer has {self.__color} color.")
+        print(f"Clock time is {self.time} hours.")
 
+
+#string 
     def __str__(self):
-        return f" A clock stop at {self.number}, the color of bell ring is {self.__color}. And the total hours of clock is {self.time} hours."
+        return f" Clock number is {self.__number}, color of bell ring is {self.__color}. And the total hours of clock is {self.time} hours."
 
+# create object of clock with time, number, color
 c1 = Clock( 24 , 12 , "black")
 
-print(c1.__color)
-
-# print(c1)
+#print the object information
+print(c1)
